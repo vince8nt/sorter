8dbe34ed2dba623b6ee727bdff44ba6bd7c0c3bd
vtittert <vtittert@ucsc.edu>
2020-07-26 17:47:19 -0700

    fix button and selector classes

--- js/sorter.js
+++ js/sorter.js
@@ -5,13 +5,6 @@ var ctx = c.getContext("2d");
 
 
 
-
-
-
-
-
-
-
 class Button {
 	constructor(x2, y2, w, h, text, col, bold) {
 		this.x = x2;
@@ -33,9 +26,11 @@ class Button {
 		ctx.fillStyle = "#000000";
 		ctx.fillText(this.label, this.x + 10, this.y + this.height / 2 + 5);
 	}
-	
 	setBold(bold) {
 		this.highlight = bold;
+	}
+	getBold() {
+		return this.highlight;
 	}
 	setColor(c) {
 		this.color = c;
@@ -47,40 +42,37 @@ class Button {
 		return this.label;
 	}
 	clicked(pointerX, pointerY) {
-		if (pointerX < this.x || this.x < pointerX + this.width)
+		if (pointerX < this.x || this.x + this.width < pointerX) {
 			return false;
-		if (pointerY < this.y || this.y < pointerY + this.height)
+		}
+		if (pointerY < this.y || this.y + this.height < pointerY) {
 			return false;
+		}
 		return true;
-	}
-	
+	}	
 }
 
-myButton = new Button(100, 100, 200, 200, "test", "#9090FF", true);
-myButton.setBold(false);
-myButton.draw();
 
-
-/*
 class Selector {
 	constructor () {
 		this.buttons = [];
 		this.bold = -1;
 	}
 	addButton (x, y, width, height, text, color) {
-		temp = new Button(x, y, width, height, text, color, false);
 		if (this.buttons.length === 0) {
-			temp.setBold(true);
+			this.buttons.push(new Button(x, y, width, height, text, color, true));
 			this.bold = 0;
 		}
-		temp.draw();
-		this.buttons.push(temp);
+		else {
+			this.buttons.push(new Button(x, y, width, height, text, color, false));
+		}
+		this.buttons[this.buttons.length - 1].draw();
 	}
 	
 	getSelected () {
 		if (this.bold === -1)
 			return "";
-		return this.buttons[this.bold].getLabel;
+		return this.buttons[this.bold].getLabel();
 	}
 	clicked (pointerX, pointerY) {
 		for (var i = 0; i < this.buttons.length; i++) {
@@ -95,19 +87,51 @@ class Selector {
 		}
 		return false;
 	}
-	
 }
-*/
 
 
-/*
+myButtonList = [];
+myButtonList.push(new Button(100, 400, 100, 150, "test", "#9090FF", false));
+myButtonList.push(new Button(250, 400, 100, 150, "test", "#9090FF", true));
+myButtonList.push(new Button(400, 400, 100, 150, "test", "#9090FF", false));
+for (var i = 0; i < myButtonList.length; i++) {
+	myButtonList[i].draw();
+}
+
+goButton = new Button(700, 200, 100, 100, "go", "#20C010", true);
+goButton.draw();
+
+
 sortType = new Selector();
 sortType.addButton(10, 10, 100, 50, "bubble", "#9090FF");
 sortType.addButton(150, 10, 100, 100, "heap", "#9090FF");
 sortType.addButton(300, 100, 200, 50, "merge", "#9090FF");
-*/
 
 
+
+
+c.addEventListener('click', function(event) {
+    var screenX = event.pageX - c.offsetLeft - c.clientLeft;
+    var screenY = event.pageY - c.offsetTop - c.clientTop;
+
+    // test buttons
+    for (var i = 0; i < myButtonList.length; i++) {
+    	if (myButtonList[i].clicked(screenX, screenY)) {
+    		myButtonList[i].setBold(!myButtonList[i].getBold());
+    		myButtonList[i].draw();
+    	}
+    }
+
+    // selector buttons
+    if (sortType.clicked(screenX, screenY))
+    	return;
+
+    // go button
+    if (goButton.clicked(screenX, screenY)) {
+    	alert("Go pressed. Using " + sortType.getSelected() + " sort.");
+    }
+
+}, false);
 
 
 

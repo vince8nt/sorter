d8e88a4f73f271d60d15c18b2b09c11add521229
vtittert <vtittert@ucsc.edu>
2020-08-28 00:11:42 -0700

    fix counting sort

--- js/sorts/valueBasedSorts.js
+++ js/sorts/valueBasedSorts.js
@@ -6,7 +6,7 @@ function countingSort(arr) {
 	
 	for (var i = 0; i < arr.length; i++) {
 		var value = getIndex(arr, i, mods);
-		auxSetIndex(aux, value - 1, auxGetIndex(aux, value - 1, mods) + 1, mods);
+		auxSetIndex(aux, value, auxGetIndex(aux, value, mods) + 1, mods);
 	}
 	for (var i = 1; i < aux.length; i++) {
 		var sum = auxGetIndex(aux, i, mods) + auxGetIndex(aux, i - 1, mods);
@@ -15,7 +15,7 @@ function countingSort(arr) {
 	var index = 0;
 	for (var i = 0; i < aux.length; i++) {
 		while (index < auxGetIndex(aux, i, mods)) {
-			setIndex(arr, index, i + 1, mods);
+			setIndex(arr, index, i, mods);
 			index++;
 		}
 	}
